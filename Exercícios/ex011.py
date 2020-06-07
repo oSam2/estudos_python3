ff74577f6ed4bcb660c7b46b0bbf79189e74f221
Sam <samuelmarques96@live.com>
2020-06-06 23:15:58 -0300

    ex011 - Pintando Parede

---
+++ Exercícios/ex011.py
@@ -0,0 +1,3 @@
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+print(f'Sua parede tem a dimensão de {larg}x{alt} e sua área é de {larg * alt}m². \nPara pintar essa parede, você precisará de {larg * alt / 2:.2f}l de tinta.')
