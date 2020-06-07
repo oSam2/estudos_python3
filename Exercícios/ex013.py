eb2582516bae90ed8b76c4eec3fa5eddc499ea33
Sam <samuelmarques96@live.com>
2020-06-06 23:41:00 -0300

    ex013 - Reajuste Salarial

---
+++ Exercícios/ex013.py
@@ -0,0 +1,3 @@
+salario = float(input('Salario do funcionário? R$'))
+aumento = salario + (salario * 15 / 100)
+print(f'Um funcionário que ganhava R${salario}, com 15% de aumento, passa a receber R${aumento:.2f} ') 
